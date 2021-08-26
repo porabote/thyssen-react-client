1ff1497aff8146888057e04c4d3876e778ead010
Denis Maksimov <you@example.com>
2021-08-26 20:38:42 +0300

    bash creator

--- .bin/creator.sh
+++ .bin/creator.sh
@@ -4,49 +4,70 @@ function renameFileOrDirByPath {
 
   path=$1
 
-  newPath=${path/Sample/Tor}
-  newPathLowerCase=${path/$sourceNameLowerCase/$targetNameLowerCase}
+  newPath=${path/${sourceName}/${targetName}};
+  newPathLowerCase=${path/$sourceNameLowerCase/$targetNameLowerCase};
 
   # checking if we need rename folder in original
   if [ "$path" != "$newPath" ]; then
     mv $path $newPath
     export current_folder="$newPath"
+    echo $newPath
   elif [ "$path" != "$newPathLowerCase" ]; then  # checking if we need rename folder in Lower case
     mv $path $newPathLowerCase
     export current_folder="$newPathLowerCase"
+    echo "${newPathLowerCase}";
 
   else
     current_folder="$path"
+    echo $path
   fi
 }
 
 function replaceFileContent {
   file=$1
+
+  echo $file;
+
   if [[ $file == *.js || $file == *.jsx ]] ; then
-    sed "s/$sourceName/$newsourceName/g" $file | sudo tee $file
-    sed "s/$sourceNameLowerCase/$newsourceNameLowerCase/g" $file | sudo tee $file
+    sed "s/$sourceName/$targetName/g" $file | sudo tee $file
+    sed "s/$sourceNameLowerCase/$targetNameLowerCase/g" $file | sudo tee $file
   fi
+}
+
+function renameDirs {
+  path="${1}/*"
+
+  #rename all dirs
+  for name in $path ; do
+        if [ -d "$name" ]; then #if it's a directory
+          renameFileOrDirByPath $name
+        fi
+  done
 }
 
 function renameFiles {
 
   path="${1}/*"
 
-  #rename all dirs and files
+  #rename all files
   for name in $path ; do
-        if [ -d "$name" ]; then #if it's a directory
-          renameFileOrDirByPath $name
-          echo "-----${name}--11${current_folder}"
-          renameFiles $current_folder
-        else #if it's a file
-          renameFileOrDirByPath $name
+        if [ ! -d "$name" ]; then #if it's not a directory
+          newName=$(renameFileOrDirByPath $name);
+          replaceFileContent "${newName}"
         fi
   done
 
+}
+
+#replace content in files
+function replaceContentInFiles {
+
+  path="${1}/*"
+
   for name in $path ; do
       if [ ! -d "$name" ]; then #if it's a file
-      echo 99
-        #replaceFileContent $name
+      echo "----${name}"
+        replaceFileContent $name
       fi
   done
 }
@@ -55,20 +76,22 @@ function renameFiles {
 function create {
 
     #set global variables
-    export sourceName="Sample";
+    export APP_ROOT="$( cd "$(dirname "../${BASH_SOURCE[0]}")" ; pwd -P )";
+    export sourceName="Reports";
     export sourceNameLowerCase=${sourceName,,};
 
     export targetName=${1}
     export targetNameLowerCase=${targetName,,};
 
-    targetFolder="$(dirname "${BASH_SOURCE[0]}")/src/components/${targetName}"
-    sourceFolder="$(dirname "${BASH_SOURCE[0]}")/src/components/${sourceName}"
+    targetFolder="${APP_ROOT}/src/components/${targetNameLowerCase}";
+    sourceFolder="${APP_ROOT}/src/components/${sourceNameLowerCase}";
 
     #clone sample folder
     cp -r $sourceFolder $targetFolder
 
+    renameDirs $targetFolder
     renameFiles $targetFolder
 }
 
 "$@"
-#creator.sh create Tor
+#bash ./.bin/creator.sh create Tor
